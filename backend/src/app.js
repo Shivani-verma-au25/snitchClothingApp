070264c6114aa5325d6c2add1732e42f24a1a186
Shivani Verma <shivani.vr.verma@gmail.com>
2026-04-14 13:24:27 +0530

    Refactor configuration import, add error handling ,response ,asyncwrapper utilities, and implement validation middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from  'cors';
 import cookieParser from  'cookie-parser';
 import morgan from 'morgan';
-import { config } from "./configrations/config";
+import { config } from "./configrations/config.js";
 
 
 const app = express();
