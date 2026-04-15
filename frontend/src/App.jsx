ff97aaca8bcb9ba6678a4a64f5aadcb07de02483
Shivani Verma <shivani.vr.verma@gmail.com>
2026-04-15 11:53:51 +0530

    feat: initialize React frontend project with Vite, Tailwind CSS, and React Router configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import { Route, Routes } from 'react-router'
 
 function App() {
 
   return (
-    <>hello</>
+    <Routes>
+      <Route path='/' element={ <h1>Hello world</h1> }/>
+    </Routes>
   )
 }
 
